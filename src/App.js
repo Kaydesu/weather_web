db49840913b151769ebae784e82ea651e6da1b46
Khoa.Phan <khoa.phan@PCX-KhoaPhan.local>
2021-04-29 13:56:37 +0700

    add hello1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     return (
         <div className="weather-app-layout">
             <NavBar />
+            hello1
             <WeatherStatistic />
             <WeatherChart />
         </div>
